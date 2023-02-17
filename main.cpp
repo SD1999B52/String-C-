7893b9a0631076b32085c053968e35ab166769cb
Danil <d.shalupin@mail.ru>
2023-02-17 17:43:05 +0500

    v0.2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
+#include <cstdlib>
 #include <iostream>
-#include "String.h"
-#include "StringStack.h"
 
 using namespace std;
 
-int main( int argc, char** argv ) {
-	String wordA("Hello");
-	String wordB(" world");
-	String wordC()
-	
-	wordB = "world";
-	
-	StringStack list;
-	list.push( wordA );
-	list.push( wordB );
-	list.out();
-	
-	system( "pause" );
-	
-	return 0;
+int main(int argc, char *argv[]) {
+    system("PAUSE");
+    
+    return EXIT_SUCCESS;
 }
