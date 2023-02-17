d5ac81bdba7405a84477969423b92a0e38490118
Danil <d.shalupin@mail.ru>
2023-02-17 17:35:13 +0500

    v0.1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "String.h"
+#include "StringStack.h"
+
+using namespace std;
+
+int main( int argc, char** argv ) {
+	String wordA("Hello");
+	String wordB(" world");
+	String wordC()
+	
+	wordB = "world";
+	
+	StringStack list;
+	list.push( wordA );
+	list.push( wordB );
+	list.out();
+	
+	system( "pause" );
+	
+	return 0;
+}
