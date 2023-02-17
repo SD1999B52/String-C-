cb0b583b461294b25b3ff60d68cc5394a5ed36fb
Danil <d.shalupin@mail.ru>
2023-02-17 20:11:59 +0500

    Редактирование Main (Тест)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    system("pause");
+    String wordA("Hello");
+	String wordB(" world");
+	String wordC();
+	
+	wordB = "world";
+	
+	StringStack list;
+	list.push(wordA);
+	list.push(wordB);
+	list.out();
+	
+	system("pause");
     
     return 0;
 }
